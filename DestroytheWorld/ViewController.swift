939bfa6e03b749783f6c08c7d98b059d7d0fd803
curiousBrahmin <Rambo@Rambos-iMac.hsd1.tn.comcast.net>
2016-09-04 07:51:55 -0500

    Hello World

--- DestroytheWorld/ViewController.swift
+++ DestroytheWorld/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(" Hello Ananth. what the heck is going on")
-        print(" todays date is")
+        print(" Hello World!" )
     }
 
     override func didReceiveMemoryWarning() {
